6a527a37d889c5509e53833994c160d9288a193f
Paweł Budek <pbudek1908@gmail.com>
2025-06-13 21:32:01 +0200

    Comment migration for postgres needs

--- database/migrations/2025_06_08_105813_change_db_charset_and_collations.php
+++ database/migrations/2025_06_08_105813_change_db_charset_and_collations.php
@@ -1,75 +1,66 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        DB::statement("ALTER TABLE `armors` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `armor_armor_location` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `armor_locations` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `characteristics` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `dynamic_footer_texts` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `failed_jobs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `footer_texts` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `heroes` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `hero_armors` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `hero_descriptions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `hero_inventory` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `hero_skill` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `hero_talent` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `hero_weapons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `jobs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `migrations` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `professions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `profession_characteristics` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `skills` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `talents` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `weapons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `weapon_traits` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-        DB::statement("ALTER TABLE `weapon_traits_weapons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-
-        DB::statement("ALTER TABLE `armors` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `armor_armor_location` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `armor_locations` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `cache` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `cache_locks` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `characteristics` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `dynamic_footer_texts` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `failed_jobs` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `footer_texts` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `heroes` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `hero_armors` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `hero_descriptions` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `hero_inventory` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `hero_skill` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `hero_talent` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `hero_weapons` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `jobs` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `job_batches` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `migrations` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `password_reset_tokens` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `professions` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `profession_characteristics` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `skills` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `sessions` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `talents` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `users` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `weapons` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `weapon_traits` ENGINE = InnoDB;");
-        DB::statement("ALTER TABLE `weapon_traits_weapons` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `armors` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `armor_armor_location` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `armor_locations` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `characteristics` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `dynamic_footer_texts` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `failed_jobs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `footer_texts` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `heroes` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `hero_armors` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `hero_descriptions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `hero_inventory` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `hero_skill` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `hero_talent` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `hero_weapons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `jobs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `migrations` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `professions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `profession_characteristics` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `skills` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `talents` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `weapons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `weapon_traits` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//        DB::statement("ALTER TABLE `weapon_traits_weapons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+//
+//        DB::statement("ALTER TABLE `armors` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `armor_armor_location` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `armor_locations` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `cache` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `cache_locks` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `characteristics` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `dynamic_footer_texts` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `failed_jobs` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `footer_texts` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `heroes` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `hero_armors` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `hero_descriptions` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `hero_inventory` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `hero_skill` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `hero_talent` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `hero_weapons` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `jobs` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `job_batches` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `migrations` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `password_reset_tokens` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `professions` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `profession_characteristics` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `skills` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `sessions` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `talents` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `users` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `weapons` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `weapon_traits` ENGINE = InnoDB;");
+//        DB::statement("ALTER TABLE `weapon_traits_weapons` ENGINE = InnoDB;");
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         //
