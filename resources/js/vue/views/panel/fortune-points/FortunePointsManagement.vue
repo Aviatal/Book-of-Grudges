3309709a5ca06c0cc95d5399814925d75b24f904
Paweł Budek <pbudek1908@gmail.com>
2025-06-17 23:06:48 +0200

    Refactor FortunePointsManagement styles for improved layout and consistency

--- resources/js/vue/views/panel/fortune-points/FortunePointsManagement.vue
+++ resources/js/vue/views/panel/fortune-points/FortunePointsManagement.vue
@@ -1,8 +1,8 @@
 <template>
-    <h1 class="flex justify-center text-[#d4af37] text-2xl mb-3">Przydziel punkty szczęścia</h1>
+    <h1 class="text-center text-[#d4af37] text-2xl mb-3">Przydziel punkty szczęścia</h1>
     <section class="flex flex-wrap gap-3 justify-center">
         <button v-for="user in activeUsers"
-                class="p-3 text-lg border-[#c09f80] border-1 border-solid rounded-sm bg-[#c29d2b] text-[#efefef] m-4"
+                class="flex-1 bg-[#c29d2b] text-lg text-[#efefef] border-[#c09f80] border rounded-md px-3 py-1.5 m-4"
                 @click="addFortunePoint(user.hero)"
         >
             {{ user.hero.name }}
