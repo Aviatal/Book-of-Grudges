bbb6eb354d21ff27068dda87cb11aa30762de54d
Paweł Budek <pbudek1908@gmail.com>
2025-06-17 22:56:23 +0200

    Schedule daily fortune points reset at 23:00

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,16 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(static function () {
+            Log::info('Updating fortune points');;
+            $heroes = \App\Models\Hero::query()->with('characteristics')->get();
+            foreach ($heroes as $hero)
+            {
+                $hero->update(['fortune_points' => $hero->characteristics['PP']->start_value]);
+            }
+        })->dailyAt('23:00');
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
