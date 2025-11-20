12a9305809e0a92073752b87467748a3c0de6984
Paweł Budek <pbudek1908@gmail.com>
2025-11-20 20:34:05 +0100

    Web socket test - disable autorization

--- routes/channels.php
+++ routes/channels.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('hero.{heroId}', static function ($user, $heroId) {
-    return $user->id === \App\Models\Hero::findOrFail($heroId)?->user_id;
+    return true;
 });
