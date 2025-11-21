b95fa40bc0c8b967968806d8211fa4d0e8f0f58e
Paweł Budek <pbudek1908@gmail.com>
2025-11-21 23:15:28 +0100

    Fix null issue in fortune points update logic and adjust schedule time to 23:17

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Schedule::call(static function () {
     $heroes = \App\Models\Hero::query()->with('characteristic')->get();
     foreach ($heroes as $hero)
     {
-        $hero->update(['fortune_points' => $hero->getRelation('characteristic')['PP']->current_value]);
+        $hero->update(['fortune_points' => $hero->getRelation('characteristic')['PP']->current_value ?? 0]);
     }
-})->dailyAt('21:35');
+})->dailyAt('23:17');
